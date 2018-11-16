8535d93b3bc166e2432a6c25b647a1ea1e48aff0
fengys <fys664099538@163.com>
2018-11-16 14:37:13 +0800

    新增testNG接口测试代码

--- Chapter1/src/main/java/com/course/cases/LoginTest.java
+++ Chapter1/src/main/java/com/course/cases/LoginTest.java
@@ -5,8 +5,14 @@ import com.course.model.InterfaceName;
 import com.course.model.LoginCase;
 import com.course.utils.ConfigFile;
 import com.course.utils.DatabaseUtil;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
 import org.apache.ibatis.session.SqlSession;
+import org.json.JSONObject;
+import org.testng.Assert;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
@@ -31,7 +37,12 @@ public class LoginTest {
         LoginCase loginCase = session.selectOne("loginCase",1);
         System.out.println(loginCase.toString());
         System.out.println(TestConfig.loginUrl);
+        //发送请求
+        String result = getResult(loginCase);
+        //验证结果
+        Assert.assertEquals(loginCase.getExpected(),result);
     }
+
 
     @Test(groups = "loginFalse",description = "用户登录失败接口")
     public void loginFalse() throws IOException {
@@ -39,5 +50,30 @@ public class LoginTest {
         LoginCase loginCase = session.selectOne("loginCase",2);
         System.out.println(loginCase.toString());
         System.out.println(TestConfig.loginUrl);
+
+        //发送请求
+        String result = getResult(loginCase);
+        //验证结果
+        Assert.assertEquals(loginCase.getExpected(),result);
+    }
+
+    private String getResult(LoginCase loginCase) throws IOException {
+
+        HttpPost post = new HttpPost(TestConfig.loginUrl);
+        JSONObject param = new JSONObject();
+        param.put("userName",loginCase.getUserName());
+        param.put("password",loginCase.getPassword());
+        //设置头信息
+        post.addHeader("content-type","application/json");
+        StringEntity entity = new StringEntity(param.toString(),"utf-8");
+        post.setEntity(entity);
+
+        String result;
+        HttpResponse response = TestConfig.defaultHttpClient.execute(post);
+        result = EntityUtils.toString(response.getEntity(),"utf-8");
+
+        TestConfig.store = TestConfig.defaultHttpClient.getCookieStore();
+
+        return result;
     }
 }
